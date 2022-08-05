ee6807a8546481601b8c47db13d5d1b5e1f8b5b5
Aiman217 <77523218+Aiman217@users.noreply.github.com>
2022-08-05 22:23:39 +0800

    Redo the project from start

--- pages/about.js
+++
@@ -1,9 +0,0 @@
-const about = () => {
-  return (
-    <div>
-      <h1 className="text-2xl font-bold text-black">About ny</h1>
-    </div>
-  );
-}
-
-export default about;
--- pages/discovery.js
+++
@@ -1,9 +0,0 @@
-const discovery = () => {
-  return (
-    <div>
-      <h1 className="text-2xl font-bold text-black">Discovery</h1>
-    </div>
-  );
-}
-
-export default discovery;
