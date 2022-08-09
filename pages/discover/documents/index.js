610ddd18c4db0a04f54306f6579984ef18a4257b
Aiman217 <77523218+Aiman217@users.noreply.github.com>
2022-08-09 18:30:49 +0800

    Fix documents page layout.

--- pages/discover/documents/index.js
+++ pages/discover/documents/index.js
@@ -39,44 +39,46 @@ const Documents = () => {
 
   return (
     <>
-      <div className="py-4 w-[90%] sm:w-[80%]">
-        <h2 className="bg-base-100 rounded-full px-3 py-1 text-lg text-center mb-4 font-bold">
-          Important Documents
-        </h2>
-        <div className="card w-full overflow-x-auto border-2 py-4">
-          <table className="table table-zebra table-compact">
-            <thead>
-              <tr>
-                <th></th>
-                <th>Name</th>
-                <th>Created At</th>
-                <th>Action</th>
-              </tr>
-            </thead>
-            <tbody>
-              {docLists.map((item, index) => (
-                <tr key={index}>
-                  <th>{++index}</th>
-                  <td>{item.name}</td>
-                  <td>
-                    {Moment.unix(item.created_at.seconds).format(
-                      "MMMM Do YYYY, h:mm:ss a"
-                    )}
-                  </td>
-                  <td>
-                    <button
-                      onClick={() => {
-                        window.open(item.url);
-                      }}
-                      className="btn btn-circle"
-                    >
-                      <AiOutlineCloudDownload size={20} />
-                    </button>
-                  </td>
+      <div className="py-4 flex flex-col justify-center items-center gap-4">
+        <div className="w-[90%] sm:w-[80%]">
+          <h2 className="bg-base-100 rounded-full px-3 py-1 text-lg text-center mb-4 font-bold">
+            Important Documents
+          </h2>
+          <div className="card w-full overflow-x-auto border-2 py-4">
+            <table className="table table-zebra table-compact">
+              <thead>
+                <tr>
+                  <th></th>
+                  <th>Name</th>
+                  <th>Created At</th>
+                  <th>Action</th>
                 </tr>
-              ))}
-            </tbody>
-          </table>
+              </thead>
+              <tbody>
+                {docLists.map((item, index) => (
+                  <tr key={index}>
+                    <th>{++index}</th>
+                    <td>{item.name}</td>
+                    <td>
+                      {Moment.unix(item.created_at.seconds).format(
+                        "MMMM Do YYYY, h:mm:ss a"
+                      )}
+                    </td>
+                    <td>
+                      <button
+                        onClick={() => {
+                          window.open(item.url);
+                        }}
+                        className="btn btn-circle"
+                      >
+                        <AiOutlineCloudDownload size={20} />
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
     </>
