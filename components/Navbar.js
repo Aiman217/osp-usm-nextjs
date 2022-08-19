243814a40109617b766bd8114d1b12a33532fed7
Aiman217 <77523218+Aiman217@users.noreply.github.com>
2022-08-19 21:36:49 +0800

    Update Discussion page.

--- components/Navbar.js
+++ components/Navbar.js
@@ -41,18 +41,18 @@ const Nav = ({ children }) => {
         <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
         <div className="drawer-content flex flex-col min-h-screen">
           <div className="navbar bg-base-100 px-4 shadow-sm shadow-neutral">
-            <div className="flex-none sm:hidden">
+            <div className="flex-none lg:hidden">
               <label htmlFor="my-drawer-3" className="btn btn-square btn-ghost">
                 <HiMenu htmlFor="my-drawer-3" size={25} />
               </label>
             </div>
             <div className="flex-1">
               <h1 className="font-bold">OneStopPortal</h1>
-              <div className="divider divider-horizontal hidden sm:block" />
-              <ThemeSelector theme="select-sm hidden sm:block" />
-              <div className="divider divider-horizontal hidden sm:block" />
+              <div className="divider divider-horizontal hidden lg:block" />
+              <ThemeSelector theme="select-sm hidden lg:block" />
+              <div className="divider divider-horizontal hidden lg:block" />
             </div>
-            <div className="flex-none hidden sm:block">
+            <div className="flex-none hidden lg:block">
               <ul className="flex flex-row gap-4">
                 <li className="uppercase font-bold">
                   <Link href="/">
@@ -86,9 +86,9 @@ const Nav = ({ children }) => {
               </ul>
             </div>
             <div className="flex-none">
-              <div className="divider divider-horizontal hidden sm:block" />
+              <div className="divider divider-horizontal hidden lg:block" />
               {_.isEmpty(user) ? (
-                <button className="btn btn-outline btn-success btn-xs sm:btn-md">
+                <button className="btn btn-outline btn-success btn-xs lg:btn-md">
                   <Link href="/login">
                     <a>Login</a>
                   </Link>
@@ -96,7 +96,7 @@ const Nav = ({ children }) => {
               ) : (
                 <button
                   onClick={logout}
-                  className="btn btn-outline btn-error btn-xs sm:btn-md"
+                  className="btn btn-outline btn-error btn-xs lg:btn-md"
                 >
                   Logout
                 </button>
@@ -112,9 +112,9 @@ const Nav = ({ children }) => {
         <div className="drawer-side">
           <label
             htmlFor="my-drawer-3"
-            className="drawer-overlay sm:hidden"
+            className="drawer-overlay lg:hidden"
           ></label>
-          <div className="menu p-4 overflow-y-auto w-[60%] bg-base-100 sm:hidden">
+          <div className="menu p-4 overflow-y-auto w-[60%] md:w-[30%] bg-base-100 lg:hidden">
             <ThemeSelector theme="select-xs" />
             <div className="divider" />
             <ul className="flex flex-col">
@@ -133,13 +133,13 @@ const Nav = ({ children }) => {
                   <a>Discover</a>
                 </Link>
               </li>
-              <li className="uppercase font-bold">
+              <li className="uppercase font-bold text-sm">
                 <Link href="/discussion">
                   <a>Discussion</a>
                 </Link>
               </li>
               {showCMS ? (
-                <li className="uppercase font-bold">
+                <li className="uppercase font-bold text-sm">
                   <Link href="/cms">
                     <a>CMS</a>
                   </Link>
--- pages/discussion/index.js
+++ pages/discussion/index.js
@@ -1,13 +1,76 @@
-import Head from 'next/head'
+import Head from "next/head";
+import { useState, useEffect } from "react";
+import { getDocs, collection } from "firebase/firestore";
+import { db } from "/firebase-config";
+import Moment from "moment";
+import Loading from "/components/Loading";
 
 const Discussion = () => {
+  const [discussList, setDiscussList] = useState([]);
+  const [discussSelect, setDiscussSelect] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Fetch announcements
+    const getDiscuss = async () => {
+      const dataDiscuss = await getDocs(collection(db, "discussion"));
+      setDiscussList(
+        dataDiscuss.docs.map((doc) => ({
+          ...doc.data(),
+          id: doc.id,
+        }))
+      );
+      setLoading(false);
+    };
+    getDiscuss();
+    // Finish fetching announcements
+  }, []);
+
   return (
     <>
       <Head>
         <title>OSP@USM | Discussion</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-      <div>Enter</div>
+      <div className="py-4 flex flex-col justify-center items-center gap-4">
+        <div className="w-[90%] sm:w-[80%]">
+          <h2 className="bg-base-100 rounded-full px-3 py-1 text-lg text-center mb-4 font-bold">
+            Discussion
+          </h2>
+          {console.log(discussList, discussSelect)}
+          <div className="card w-full overflow-x-auto border-2 py-4">
+            <table className="table table-zebra table-compact">
+            <thead>
+                <tr>
+                  <th></th>
+                  <th>Title</th>
+                  <th>Desc</th>
+                  <th>Created At</th>
+                  <th>Upvote</th>
+                </tr>
+              </thead>
+              <tbody>
+                {discussList.map((item, index) => (
+                  <tr key={index} onClick={() => {
+                    setDiscussSelect(item)
+                  }}>
+                    <th>{++index}</th>
+                    <td>{item.title}</td>
+                    <td>{item.desc.substring(0, 24) + "..."}</td>
+                    <td>
+                      {Moment.unix(item.created_at.seconds).format(
+                        "MMMM Do YYYY, h:mm:ss a"
+                      )}
+                    </td>
+                    <td>{item.upvote}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+      {loading && <Loading />}
     </>
   );
 };
