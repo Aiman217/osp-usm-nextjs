5661064ec53e15acabbc7db226d8b717a462caa3
Aiman217 <77523218+Aiman217@users.noreply.github.com>
2022-08-25 22:31:33 +0800

    Add search support for Links page.

--- pages/discover/links/index.jsx
+++ pages/discover/links/index.jsx
@@ -5,9 +5,11 @@ import { getDoc, doc } from "firebase/firestore";
 import { db } from "/firebase-config";
 import { AiOutlineLink } from "react-icons/ai";
 import Loading from "/components/Loading";
+import Search from "/components/Search";
 
 const Links = () => {
   const [linkLists, setLinkLists] = useState([]);
+  const [linkSearch, setLinkSearch] = useState([]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
@@ -15,6 +17,7 @@ const Links = () => {
     const getLinks = async () => {
       const dataLink = await getDoc(doc(db, "contents", "links"));
       setLinkLists(Object.values(dataLink.data()));
+      setLinkSearch(Object.values(dataLink.data()));
       setLoading(false);
     };
     getLinks();
@@ -29,15 +32,23 @@ const Links = () => {
       </Head>
       <div className="py-4 flex flex-col justify-center items-center gap-4">
         <div className="w-[90%] sm:w-[80%]">
-          <div className="text-sm breadcrumbs">
-            <ul>
-              <li>
-                <Link href="/discover">Discover</Link>
-              </li>
-              <li>Links</li>
-            </ul>
+          <div className="flex flex-col md:flex-row justify-between items-center">
+            <div className="text-sm breadcrumbs">
+              <ul>
+                <li>
+                  <Link href="/discover">Discover</Link>
+                </li>
+                <li>Links</li>
+              </ul>
+            </div>
+            <Search
+              searchList={linkSearch}
+              setSearchList={setLinkSearch}
+              backupList={linkLists}
+              type="owner"
+            />
           </div>
-          <h2 className="bg-base-100 rounded-full px-3 py-1 text-lg text-center mb-4 font-bold">
+          <h2 className="bg-base-100 rounded-full px-3 py-1 text-lg text-center my-4 font-bold">
             Important Links
           </h2>
           <div className="card w-full overflow-x-auto border-2 py-4">
@@ -51,22 +62,21 @@ const Links = () => {
                 </tr>
               </thead>
               <tbody>
-                {linkLists.map((item, index) => (
+                {linkSearch.map((item, index) => (
                   <tr key={index}>
                     <th>{++index}</th>
                     <td>{item.owner}</td>
                     <td>{item.desc.substring(0, 40) + "..."}</td>
                     <td>
                       <div className="tooltip" data-tip="Go To Link">
-
-                      <button
-                        onClick={() => {
-                          window.open(item.url);
-                        }}
-                        className="btn btn-circle"
-                      >
-                        <AiOutlineLink size={20} />
-                      </button>
+                        <button
+                          onClick={() => {
+                            window.open(item.url);
+                          }}
+                          className="btn btn-circle"
+                        >
+                          <AiOutlineLink size={20} />
+                        </button>
                       </div>
                     </td>
                   </tr>
